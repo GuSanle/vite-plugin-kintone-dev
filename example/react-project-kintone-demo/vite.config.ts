02778ca541e12fa5065415d76f4d2d96c27fa862
slgu <slgu@cybozu.cn>
2023-09-05 22:29:24 +0800

    react template init

---
+++ example/react-project-kintone-demo/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import kintoneDev from "vite-plugin-kintone-dev";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
