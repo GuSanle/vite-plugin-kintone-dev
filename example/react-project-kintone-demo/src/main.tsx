160d7cc65277580b496997dcf22273826078cba8
slgu <slgu@cybozu.cn>
2023-09-06 18:20:25 +0800

    react example

--- example/react-project-kintone-demo/src/main.tsx
+++ example/react-project-kintone-demo/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+kintone.events.on("portal.show", (event) => {
+  ReactDOM.createRoot(kintone.portal.getContentSpaceElement()!).render(<App />);
+  return event;
+});
+// ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- example/vue3-porject-kintone-demo/src/main.ts
+++ example/vue3-porject-kintone-demo/src/main.ts
@@ -5,11 +5,8 @@ import App from "./App.vue";
 import router from "./router";
 
 kintone.events.on("portal.show", (event) => {
-  const contentSpaceElement = kintone.portal.getContentSpaceElement();
   const app = createApp(App);
   app.use(router);
-  if (contentSpaceElement) {
-    app.mount(contentSpaceElement);
-  }
+  app.mount(kintone.portal.getContentSpaceElement()!);
   return event;
 });
