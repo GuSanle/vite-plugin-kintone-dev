dfc9895dc8847fa104bdb49b4fded4759b13e33b
slgu <slgu@cybozu.cn>
2023-09-06 20:17:01 +0800

    react example
    react example

--- example/react-project-kintone-demo/vite.config.ts
+++ example/react-project-kintone-demo/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import kintoneDev from "vite-plugin-kintone-dev";
+// import kintoneDev from "./vite-plugin/index";
+import kintoneDev from "vite-plugin-kintone-dev";
 
+type TypeInput = {
+  platform: "APP" | "PORTAL";
+  type: "DESKTOP" | "MOBILE";
+};
+
+const inputType: TypeInput = {
+  platform: "PORTAL",
+  type: "DESKTOP",
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), kintoneDev(inputType)],
 });
