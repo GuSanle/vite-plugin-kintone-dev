7dab8d61272bae1da3eec952312d18421b56dc5d
slgu <slgu@cybozu.cn>
2023-09-06 18:24:44 +0800

    react example2

--- example/react-project-kintone-demo/src/App.tsx
+++ example/react-project-kintone-demo/src/App.tsx
@@ -1,18 +1,7 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 export default function () {
   // return <h1>Hello</h1>
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
     </>
   )
