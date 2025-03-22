8dfb52131b53acd974bf667bac2ac31817913bcc
slgu <slgu@cybozu.cn>
2025-03-22 10:07:53 +0800

    demo update

--- example/react-kintone-vite-demo/src/main.tsx
+++ example/react-kintone-vite-demo/src/main.tsx
@@ -2,16 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 // import './index.css'
 
-
-kintone.events.on("portal.show", (event) => {
-  ReactDOM.createRoot(kintone.portal.getContentSpaceElement()!).render(<App />);
-  return event;
-});
-
-// kintone.events.on("app.record.detail.show", (event) => {
-//   ReactDOM.createRoot(kintone.app.record.getHeaderMenuSpaceElement()!).render(<App />);
+// kintone.events.on("portal.show", (event) => {
+//   ReactDOM.createRoot(kintone.portal.getContentSpaceElement()!).render(<App />);
 //   return event;
 // });
+
+kintone.events.on('app.record.detail.show', (event) => {
+  ReactDOM.createRoot(kintone.app.record.getHeaderMenuSpaceElement()!).render(<App />)
+  return event
+})
 
 // const event = new Event("load");
 // // @ts-ignore
